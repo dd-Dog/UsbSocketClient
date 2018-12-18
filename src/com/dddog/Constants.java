796ef84a4078011f853e278d4f00960c8be2b296
bianjb <bianjb@flyscale.cn>
2018-12-18 15:42:16 +0800

    重新整理了，ADB转发，连接，通信，断开连接，移除转发的流程

--- src/com/dddog/Constants.java
+++ src/com/dddog/Constants.java
@@ -1,9 +1,16 @@
 package com.dddog;
 
+import java.awt.Color;
 import java.awt.Font;
 
 public class Constants {
 	public static final int FRAME_WIDHT = 960;
 	public static final int FRAME_HEIGHT = 640;
-	public static final Font DEFAULT_FONT = new Font("", Font.PLAIN, 16) ;
+	public static final Font DEFAULT_FONT = new Font("", Font.PLAIN, 18) ;
+	public static final String LOCAL_PORT = "8000";
+	public static final String SERVER_PORT = "9000";
+	
+	//R,G,B,A
+	public static final Color DARK_GREEN = new Color(0, 139, 69, 0);
+	public static final Color DARK_RED = new Color(178, 34, 34, 0);
 }
--- src/com/dddog/USBSocketClient.java
+++ src/com/dddog/USBSocketClient.java
@@ -28,6 +28,7 @@ import java.util.Scanner;
 
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 import org.json.JSONObject;
 
@@ -39,16 +40,21 @@ public class USBSocketClient implements ActionListener {
 	private static USBSocketClient mClient;
 	Button mConnBtn;
 	Button mSendBtn;
-	Button mClearBtn;
+	Button mClearHistoryBtn;
+	Button mClearCmdBtn;
 	Button mForwardBtn;
-	TextField mCmdTF;
+	JTextField mCmdTF;
 	TextField mLocalPortTF;
 	TextField mRemotePortTF;
 	static JTextArea mInfo;
-	Label mStatus;
+	Label mForwardStatus;
+	Label mConnStatus;
 	Socket mSocket;
-	private static boolean mConnectStatus = false;
+	private static boolean mConnectStatus = false;//SOCKET连接状态
+	private boolean mForwardSuccess = false; //adb端口转发状态
 	ListenServerThread mListenServerThread;
+	String mLocalPort;
+	String mServerPort;
 
 	public static void main(String[] args) throws IOException {
 
@@ -83,21 +89,22 @@ public class USBSocketClient implements ActionListener {
 		GridBagConstraints gbc = new GridBagConstraints();
 		panel.setLayout(gbl);
 
-		mForwardBtn = new Button("ADB转发端口");
+		mForwardBtn = new Button("转发端口");
 		mForwardBtn.setFont(Constants.DEFAULT_FONT);
 		mConnBtn = new Button("打开连接");
 		mConnBtn.setFont(Constants.DEFAULT_FONT);
 		mSendBtn = new Button("发送");
 		mSendBtn.setFont(Constants.DEFAULT_FONT);
-		Label localPortLabel = new Label("本地端口：",Label.CENTER );
+		Label localPortLabel = new Label("本地端口：", Label.CENTER);
 		localPortLabel.setFont(Constants.DEFAULT_FONT);
-		Label remotePortLabel = new Label("服务器端口：",Label.CENTER);
+		Label remotePortLabel = new Label("服务器端口：", Label.CENTER);
 		remotePortLabel.setFont(Constants.DEFAULT_FONT);
-		mLocalPortTF = new TextField();
+		mLocalPortTF = new TextField(Constants.LOCAL_PORT);
 		mLocalPortTF.setFont(Constants.DEFAULT_FONT);
-		mRemotePortTF = new TextField();
+		mRemotePortTF = new TextField(Constants.SERVER_PORT);
 		mRemotePortTF.setFont(Constants.DEFAULT_FONT);
-		mCmdTF = new TextField();
+		mCmdTF = new JTextField();
+		JScrollPane jsp2 = new JScrollPane(mCmdTF);
 		mCmdTF.setFont(Constants.DEFAULT_FONT);
 		mInfo = new JTextArea();
 		mInfo.setLineWrap(true);
@@ -107,24 +114,35 @@ public class USBSocketClient implements ActionListener {
 		//使用JScrollPane包裹TextArea
 		JScrollPane jsp1 = new JScrollPane(mInfo);
 
-		mClearBtn = new Button("清除窗口");
-		mClearBtn.setFont(Constants.DEFAULT_FONT);
+		mClearHistoryBtn = new Button("清空记录");
+		mClearHistoryBtn.setFont(Constants.DEFAULT_FONT);
+		
+		mClearCmdBtn = new Button("清空指令");
+		mClearCmdBtn.setFont(Constants.DEFAULT_FONT);
 
-		mStatus = new Label("DISCONNECTED", Label.CENTER);
-		mStatus.setFont(new Font("", Font.BOLD, 20));
-		mStatus.setForeground(Color.RED);
+		mForwardStatus = new Label("ADB未转发", Label.CENTER);
+		mForwardStatus.setFont(new Font("", Font.BOLD, 20));
+		mForwardStatus.setForeground(Constants.DARK_RED);
+		
+		mConnStatus = new Label("SOCKET未连接", Label.CENTER);
+		mConnStatus.setFont(new Font("", Font.BOLD, 20));
+		mConnStatus.setForeground(Constants.DARK_RED);
 
+		mForwardBtn.addActionListener(this);
 		mConnBtn.addActionListener(this);
 		mSendBtn.addActionListener(this);
-		mClearBtn.addActionListener(this);
+		mClearHistoryBtn.addActionListener(this);
+		mClearCmdBtn.addActionListener(this);
 
-		panel.add(mStatus);
+		panel.add(mForwardStatus);
+		panel.add(mConnStatus);
 		panel.add(mConnBtn);
-		panel.add(mCmdTF);
+		panel.add(jsp2);
 		panel.add(mSendBtn);
 		panel.add(jsp1);
-		panel.add(mClearBtn);
-		
+		panel.add(mClearHistoryBtn);
+		panel.add(mClearCmdBtn);
+
 		panel.add(mForwardBtn);
 		panel.add(localPortLabel);
 		panel.add(remotePortLabel);
@@ -132,7 +150,8 @@ public class USBSocketClient implements ActionListener {
 		panel.add(mRemotePortTF);
 
 		//连接状态显示区域
-		setConstraints(gbl, gbc, mStatus, GridBagConstraints.BOTH, 4, 1, 0, 0, 1, 0, null);
+		setConstraints(gbl, gbc, mForwardStatus, GridBagConstraints.BOTH, 2, 1, 0, 0, 1, 0, null);
+		setConstraints(gbl, gbc, mConnStatus, GridBagConstraints.BOTH, 2, 1, 2, 0, 1, 0, null);
 
 		//端口信息
 		setConstraints(gbl, gbc, localPortLabel, GridBagConstraints.BOTH, 1, 1, 0, 1, 1, 0, null);
@@ -140,18 +159,19 @@ public class USBSocketClient implements ActionListener {
 		setConstraints(gbl, gbc, remotePortLabel, GridBagConstraints.BOTH, 1, 1, 2, 1, 1, 0, null);
 		setConstraints(gbl, gbc, mRemotePortTF, GridBagConstraints.BOTH, 1, 1, 3, 1, 1, 0, null);
 		setConstraints(gbl, gbc, mForwardBtn, GridBagConstraints.BOTH, 1, 1, 4, 1, 1, 0, null);
-		
+
 		//连接按钮
 		setConstraints(gbl, gbc, mConnBtn, GridBagConstraints.BOTH, 1, 1, 4, 2, 1, 0, null);
 
 		//输入指令区域
-		setConstraints(gbl, gbc, mCmdTF, GridBagConstraints.BOTH, 4, 2, 0, 2, 1, 0, null);
+		setConstraints(gbl, gbc, jsp2, GridBagConstraints.BOTH, 4, 2, 0, 2, 1, 0, null);
 		//发送按钮
 		setConstraints(gbl, gbc, mSendBtn, GridBagConstraints.BOTH, 1, 1, 4, 3, 1, 0, null);
 		//清除按钮
-		setConstraints(gbl, gbc, mClearBtn, GridBagConstraints.HORIZONTAL, 1, 1, 4, 4, 1, 0, null);
+		setConstraints(gbl, gbc, mClearCmdBtn, GridBagConstraints.HORIZONTAL, 1, 1, 4, 4, 1, 0, null);
+		setConstraints(gbl, gbc, mClearHistoryBtn, GridBagConstraints.HORIZONTAL, 1, 1, 4, 5, 1, 0, null);
 		//展示信息
-		setConstraints(gbl, gbc, jsp1, GridBagConstraints.BOTH, 4, 1, 0, 4, 2, 1, null);
+		setConstraints(gbl, gbc, jsp1, GridBagConstraints.BOTH, 4, 10, 0, 4, 2, 1, null);
 		return panel;
 	}
 
@@ -184,21 +204,56 @@ public class USBSocketClient implements ActionListener {
 		gbl.setConstraints(comp, gbc);
 	}
 
-	private void start() {
-		if (!setupAdbForward()) {
-			mConnectStatus = false;
-			printPanel("设置端口转发失败！");
-			mStatus.setText("DISCONNECTED");
-			mStatus.setForeground(Color.RED);
-			mConnBtn.setLabel("打开连接");
+	/**
+	 * 连接到服务器
+	 */
+	private void connect2Server() {
+		if (!mForwardSuccess) {
+			printPanel("connect2Server failed, no create forward!");
 			return;
 		}
-		mConnectStatus = true;
-		printPanel("设置端口转发成功！");
-		mStatus.setText("CONNECTED");
-		mStatus.setForeground(Color.GREEN);
-		mConnBtn.setLabel("断开连接");
-		listenServer();
+		try {
+			if (mSocket == null ||(mSocket!=null && mSocket.isClosed())) {
+				if (mLocalPort != null && !mLocalPort.equals("")) {
+					int localPort = Integer.parseInt(mLocalPort);
+					mSocket = new Socket("127.0.0.1", localPort);
+				}
+			}
+			byte[] buffer = new byte[256];
+			DataInputStream dis = new DataInputStream(mSocket.getInputStream());
+			DataOutputStream dos = new DataOutputStream(mSocket.getOutputStream());
+
+			String msg = "Hello,this is client!";
+			// 发送数据
+			printPanel("Client:" + msg);
+			dos.write(msg.getBytes("UTF-8"));
+			dos.flush();
+
+			//读取回应
+			int len = dis.read(buffer);
+			if (len < 0) {
+				printPanel("Server not response!");
+				mConnectStatus = false;
+			} else {
+				mConnectStatus = true;
+				String ack = new String(buffer, 0, len);
+				printPanel("Server:" + ack);
+				printPanel("Server connected!");
+				listenServer();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			mConnectStatus = false;
+			if (mSocket != null) {
+				try {
+					mSocket.close();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+			}
+		} finally {
+			updateConnectStatus();
+		}
 	}
 
 	/**
@@ -214,23 +269,29 @@ public class USBSocketClient implements ActionListener {
 		public void run() {
 			try {
 				while (mConnectStatus) {
-					if (mSocket == null) {
-						mSocket = new Socket("127.0.0.1", 8000);
+					if (mSocket == null ||(mSocket!=null && mSocket.isClosed())) {
+						if (mLocalPort != null && !mLocalPort.equals("")) {
+							int localPort = Integer.parseInt(mLocalPort);
+							mSocket = new Socket("127.0.0.1", localPort);
+						}
 					}
 					byte[] buffer = new byte[256];
 					DataInputStream dis = new DataInputStream(mSocket.getInputStream());
-					int len = dis.read(buffer);
-					if (len > 0) {
-						String recevStr = new String(buffer, 0, len, "UTF-8");
-						printPanel("Server:" + recevStr);
-						if (JsonUtil.isJson(recevStr, 0)) {
-							JSONObject jo = new JSONObject(recevStr);
-							String eventType = jo.getString("EventType");
-							jo.put("host", "client");
-							if (eventType.equals("101")) {
-								sendMsg(jo.toString());
-								printPanel("Client:" + jo.toString());
-							}
+					int len = -1;
+					StringBuilder sb = new StringBuilder();
+					while ((len = dis.read(buffer)) != -1) {
+						String str = new String(buffer, 0, len, "UTF-8");
+						sb.append(str);
+					}
+					String recevStr = sb.toString();
+					printPanel("Server:" + recevStr);
+					if (JsonUtil.isJson(recevStr, 0)) {
+						JSONObject jo = new JSONObject(recevStr);
+						String eventType = jo.getString("EventType");
+						jo.put("host", "client");
+						if (eventType.equals("101")) { //101是服务器的心跳连接
+							sendMsg(jo.toString());
+							printPanel("Client:" + jo.toString());
 						}
 					}
 				}
@@ -240,13 +301,75 @@ public class USBSocketClient implements ActionListener {
 		}
 	}
 
-	private boolean setupAdbForward() {
+	private void updateConnectStatus() {
+		if (!mForwardSuccess) {
+			mConnectStatus = false;
+		}
+		printPanel(mConnectStatus ? "SOCKET连接成功！" : "SOCKET未连接！");
+		mConnStatus.setText(mConnectStatus ? "SOCKET连接成功" : "SOCKET未连接");
+		mConnStatus.setForeground(mConnectStatus ? Constants.DARK_GREEN : Constants.DARK_RED);
+		mConnBtn.setLabel(mConnectStatus ? "断开连接" : "开始连接");
+	}
+
+	private void updateForwardStatus() {
+		printPanel(mForwardSuccess ? "端口转发成功！" : "未转发！");
+		mForwardStatus.setText(mForwardSuccess ? "ADB转发成功" : "ADB未转发");
+		mForwardStatus.setForeground(mForwardSuccess ? Constants.DARK_GREEN : Constants.DARK_RED);
+		mForwardBtn.setLabel(mForwardSuccess ? "移除转发" : "转发端口");
+		updateConnectStatus();
+	}
+
+	/**
+	 * 移除转发
+	 * @param localPort
+	 */
+	private void removeAdbForward(String localPort) {
 		try {
-			// PC上所有8000端口通信数据将被重定向到手机端9000端口server上
-			Runtime.getRuntime().exec("adb forward tcp:8000 tcp:9000");
+			Runtime.getRuntime().exec("adb forward --remove tcp:" + localPort);
+			mForwardSuccess = false;
+			printPanel("adb forward remove port" + localPort);
+			updateForwardStatus();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 建立转发
+	 * @param localPort	本地端口
+	 * @param serverPort	服务器端口
+	 * @return
+	 */
+	private boolean setupAdbForward(String localPort, String serverPort) {
+		try {
+			mForwardSuccess = false;
+			// PC机localPort端口通信数据将被重定向到手机端serverPort端口server上
+			Runtime.getRuntime().exec("adb forward tcp:" + localPort + " tcp:" + serverPort);
+			//读取结果，判断是否转发成功
+			Process process = Runtime.getRuntime().exec("adb forward --list");
+			DataInputStream dis = new DataInputStream(process.getInputStream());
+			byte[] buf = new byte[8];
+			int len = -1;
+			StringBuilder sb = new StringBuilder();
+			while ((len = dis.read(buf)) != -1) {
+				String str = new String(buf, 0, len);
+				sb.append(str);
+			}
+			String adbList = sb.toString().toString();
+			printPanel("adb forward list=" + adbList);
+			String[] forwardArr = adbList.split("\n");
+			for (String forward : forwardArr) {
+				printPanel("forward=" + forward);
+				if (forward.contains(localPort) && forward.contains(serverPort)) {
+					mForwardSuccess = true;
+				}
+			}
 			return true;
 		} catch (IOException e) {
 			e.printStackTrace();
+		} finally {
+			updateForwardStatus();
+
 		}
 		return false;
 	}
@@ -293,10 +416,27 @@ public class USBSocketClient implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent event) {
-		if (event.getSource() == mConnBtn) {
+		if (event.getSource() == mForwardBtn) {
+			if (mForwardSuccess) {
+				if (mConnectStatus) {
+					printPanel("请先断开连接 ！");
+				} else {
+					removeAdbForward(mLocalPort);
+				}
+			} else {
+				mLocalPort = mLocalPortTF.getText().trim();
+				mServerPort = mRemotePortTF.getText().trim();
+				setupAdbForward(mLocalPort, mServerPort);
+			}
+
+		} else if (event.getSource() == mConnBtn) {
+			if (!mForwardSuccess) {
+				printPanel("请先建立转发！");
+				return;
+			}
 			if (!mConnectStatus) {
 				printPanel("start to connect server...");
-				start();
+				connect2Server();
 			} else {
 				printPanel("disconnect link to server...");
 				if (mSocket != null) {
@@ -305,15 +445,21 @@ public class USBSocketClient implements ActionListener {
 						mListenServerThread.stop();
 						if (!mSocket.isClosed())
 							mSocket.close();
-						mStatus.setForeground(Color.RED);
-						mStatus.setText("DISCONNECTED");
-						mConnBtn.setLabel("打开连接");
+						updateConnectStatus();
 					} catch (IOException e) {
 						e.printStackTrace();
 					}
 				}
 			}
 		} else if (event.getSource() == mSendBtn) {
+			if (!mForwardSuccess) {
+				printPanel("请先建立转发！");
+				return;
+			}
+			if (!mConnectStatus) {
+				printPanel("请先建立连接！");
+				return;
+			}
 			printPanel("send msg to server");
 			String cmdStr = mCmdTF.getText();
 			if (!cmdStr.isEmpty()) {
@@ -321,8 +467,12 @@ public class USBSocketClient implements ActionListener {
 			} else {
 				printPanel("do not send empty body!");
 			}
-		} else if (event.getSource() == mClearBtn) {
+		} else if (event.getSource() == mClearHistoryBtn) {
 			mInfo.setText("");
+		}else if(event.getSource() == mClearCmdBtn) {
+			//TextFiled不允许设置为空
+			mCmdTF.setText(" ");
+			JTextField jtf;
 		}
 	}
 
